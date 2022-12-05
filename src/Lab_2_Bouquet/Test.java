5152ff2802d2a743afde6ddf22c486f84797c272
HuKuTo3 <nvorlych@gmail.com>
2022-12-05 18:27:23 +0300

    GG

--- src/Lab_2_Bouquet/Test.java
+++ src/Lab_2_Bouquet/Test.java
@@ -11,25 +11,9 @@ public class Test {
         Bouquet bouquet = new Bouquet();
         bouquet.addNewFlower(0);
         bouquet.addNewFlower(1);
-        bouquet.addNewFlower(2);
         System.out.println(bouquet);
         bouquet.sortBouquetByFreshness();
         System.out.println(bouquet);
         System.out.println(bouquet.getFlowerInRange(10, 50));
-
-
-
-
-//        FactoryBouquet bouquet = new Bouquet();
-//        bouquet.addFlower(new Daisy());
-//        bouquet.addFlower(new Carnation());
-//        bouquet.addFlower(new Sunflower());
-//        System.out.println(bouquet);
-//        bouquet.sortBouquetByFreshness();
-//        System.out.println(bouquet);
-//        bouquet.addAccessory(new Tape());
-//        bouquet.addAccessory(new Wrapping());
-//        System.out.println(bouquet);
-//        System.out.println(bouquet.getFlowerInRange(10, 50));
     }
 }
