54486e0c7d0b8b45508a8653760ee686478c8c43
HuKuTo3 <nvorlych@gmail.com>
2022-12-05 18:26:44 +0300

    GG

--- src/Lab_2_Bouquet/accessories/Tape.java
+++ src/Lab_2_Bouquet/accessories/Tape.java
@@ -1,14 +1,49 @@
 package Lab_2_Bouquet.accessories;
 
 public class Tape extends Accessory {
+    private int length;
+    private Color color;
+
+    public enum Color {
+        RED,
+        BLUE,
+        YELLOW,
+        GREEN,
+        BLACK
+    }
+
     public Tape() {
-        this.price = 5;
+        this(5, 30, Color.RED);
+    }
+
+    public Tape(int price, int length, Color color) {
+        this.price = price;
+        this.length = length;
+        this.color = color;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 
     @Override
     public String toString() {
         return "Tape{" +
-                "price=" + price +
-                "}\n";
+                "length=" + length +
+                ", color=" + color +
+                ", price=" + price +
+                '}';
     }
 }
--- src/Lab_2_Bouquet/accessories/Wrapping.java
+++ src/Lab_2_Bouquet/accessories/Wrapping.java
@@ -1,14 +1,37 @@
 package Lab_2_Bouquet.accessories;
 
 public class Wrapping extends Accessory {
+    private Material material;
+
+    public enum Material {
+        PAPER,
+        FILM,
+        GRID,
+        FELT
+    }
+
     public Wrapping() {
-        this.price = 20;
+        this(20, Material.PAPER);
+    }
+
+    public Wrapping(int price, Material material) {
+        this.price = price;
+        this.material = material;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
     }
 
     @Override
     public String toString() {
         return "Wrapping{" +
-                "price=" + price +
-                "}\n";
+                "material=" + material +
+                ", price=" + price +
+                '}';
     }
 }
